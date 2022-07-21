70d62bc0f9c35cd962fe4a181611e1d264fa0abc
Connos91 <101891082+Connos91@users.noreply.github.com>
2022-07-22 00:17:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useInfiniteQuery } from "react-query";
-import ImageItem from "./components/ImageItem";
+import ImageItem from "../components/ImageItem";
 import ImageList from "@mui/material/ImageList";
 import { createTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import InfiniteScroll from "react-infinite-scroller";
+
+import * as C from "./constants";
+
+import usePhotos from "./usePhotos";
 
 const theme = createTheme({
   breakpoints: {
@@ -17,27 +21,25 @@ const theme = createTheme({
   },
 });
 
-const App = () => {
+const Gallery = () => {
   const isMobileSmall = useMediaQuery(theme.breakpoints.down("xs"));
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const isTablet = useMediaQuery(theme.breakpoints.down("md"));
   const isDesktop = useMediaQuery(theme.breakpoints.down("lg"));
 
-  const fetchPhotos = async ({ pageParam = 1 }) => {
-    const response = await fetch(
-      `https://picsum.photos/v2/list?page=${pageParam}&limit=10`
-    );
-    const results = await response.json();
-    return { results, nextPage: pageParam + 1, totalPages: 100 };
-  };
+  const {
+    data = [],
+    isLoading,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+  } = usePhotos(C.pageParam, C.limit);
 
-  const { data, isLoading, isError, hasNextPage, fetchNextPage } =
-    useInfiniteQuery("photos", fetchPhotos, {
-      getNextPageParam: (lastPage, pages) => {
-        if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
-        return undefined;
-      },
-    });
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      fetchNextPage();
+    }, 5000);
+  };
 
   return (
     <>
@@ -46,31 +48,43 @@ const App = () => {
       ) : isError ? (
         <p>There was an error</p>
       ) : (
-        <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
-          <ImageList
-            variant="masonry"
-            cols={
-              isMobileSmall
-                ? 1
-                : isMobile
-                ? 1
-                : isTablet
-                ? 2
-                : isDesktop
-                ? 3
-                : 3
+        <>
+          <InfiniteScroll
+            pageStart={0}
+            initialLoad={false}
+            hasMore={hasNextPage}
+            loadMore={fetchMoreData}
+            loader={
+              <div className="loader" key={0}>
+                Loading ...
+              </div>
             }
-            gap={8}
           >
-            {data.pages.map((page) =>
-              page.results.map((post) => (
-                <ImageItem key={post.id} photo={post} />
-              ))
-            )}
-          </ImageList>
-        </InfiniteScroll>
+            <ImageList
+              variant="masonry"
+              cols={
+                isMobileSmall
+                  ? 1
+                  : isMobile
+                  ? 1
+                  : isTablet
+                  ? 2
+                  : isDesktop
+                  ? 3
+                  : 3
+              }
+              gap={8}
+            >
+              {data?.pages.map((page) =>
+                page?.response.map((post) => (
+                  <ImageItem key={post.id} photo={post} />
+                ))
+              )}
+            </ImageList>
+          </InfiniteScroll>
+        </>
       )}
     </>
   );
 };
-export default App;
+export default Gallery;
